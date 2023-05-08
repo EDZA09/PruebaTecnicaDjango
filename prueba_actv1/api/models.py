f30c3e1f1874796423edfaaa6234872def8dac8c
EDZA09 <eduarstiven2001@gmail.com>
2023-05-08 02:35:44 -0500

    Creando modelo de usuario

--- prueba_actv1/api/models.py
+++ prueba_actv1/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#* Modelo de Usuario
+  
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    #models.t
+    boton1 = models.IntegerField(default=0)
+    boton2 = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{0},{1}'.format(self.usuario.last_name,self.usuario.first_name)
+
--- prueba_actv1/api/views.py
+++ prueba_actv1/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from .forms import UserRegisterForm, PostForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+#class UsuarioView(
+
+def register(request):
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			messages.success(request, f'Usuario {username} creado')
+			return redirect('feed')
+	else:
+		form = UserRegisterForm()
+
+	context = { 'form' : form }
+	return render(request, 'social/register.html', context)
