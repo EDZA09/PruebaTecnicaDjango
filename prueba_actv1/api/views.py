e9b14cbc45af2fe610bafd5cc615078f1483bd27
EDZA09 <eduarstiven2001@gmail.com>
2023-05-09 17:27:07 -0500

    Creando Templates - login, logout y register

--- prueba_actv1/api/views.py
+++ prueba_actv1/api/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-from .forms import UserRegisterForm, PostForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 #class UsuarioView(
+
+def home(request):
+
+	return render(request, './home.html')
 
 def register(request):
 	if request.method == 'POST':
@@ -15,9 +19,9 @@ def register(request):
 			form.save()
 			username = form.cleaned_data['username']
 			messages.success(request, f'Usuario {username} creado')
-			return redirect('feed')
+			return redirect('Home')
 	else:
 		form = UserRegisterForm()
 
 	context = { 'form' : form }
-	return render(request, 'social/register.html', context)
+	return render(request, 'templates/register.html', context)
